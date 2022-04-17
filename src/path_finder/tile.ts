4bcde7fca6f9787c9ecb5174d0c1ec79c2075c09
WillYallop <hello@williamyallop.com>
2022-04-17 22:31:43 +0100

    [style]: started work on basic styling and layout

--- src/path_finder/tile.ts
+++ src/path_finder/tile.ts
@@ -8,10 +8,18 @@ export default class Tile {
 
     render(container: HTMLElement) {
         this.element = document.createElement('div');
-        this.element.classList.add('canvas__tile');
+        this.element.classList.add(`canvas__tile`);
+        this.setTileState(this.state.state);
 
-        this.element.innerHTML = `<div class="canvas__tile__inner">${this.state.x}, ${this.state.y}</div>`;
+        this.element.setAttribute('data-x', this.state.x.toString());
+        this.element.setAttribute('data-y', this.state.y.toString());
 
         container.append(this.element);
+    }
+
+    setTileState(state: class_ti_state) {
+        this.element.classList.remove(`canvas__tile--${this.state.state}`);
+        this.state.state = state;
+        this.element.classList.add(`canvas__tile--${this.state.state}`);
     }
 }
